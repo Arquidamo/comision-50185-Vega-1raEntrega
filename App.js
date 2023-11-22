1d9db3b757a0ce6e18a3cc2659995f26a56f78c5
Arquidamo de Esparta <Arquidamo de Esparta@gmail.com>
2023-11-21 22:48:50 -0300

    primer commit

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>T hings
+            A re
+            R eally
+            F ucked
+            U p 
+            N ow
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
